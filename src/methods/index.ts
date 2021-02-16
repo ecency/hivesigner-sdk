b4a68aad4790a2d458c172b7d659a745b570f0b7
Ildar Timerbaev <ildar.timerbaev@piano.io>
2021-02-17 00:45:20 +0300

    Added send operation method

--- src/methods/index.ts
+++ src/methods/index.ts
@@ -1,1 +1,2 @@
 export * from './sign.method'
+export * from './send-operation.method'
---
+++ src/methods/send-operation.method.ts
@@ -0,0 +1,13 @@
+import { BETA_URL } from '../consts'
+import { encodeOp, Parameters } from 'hive-uri'
+import { isBrowser } from '../utilities'
+
+export function sendOperation(op: any, params: Parameters, cb: Function) {
+	const uri = encodeOp(op, params)
+	const webUrl = uri.replace('hive://', `${BETA_URL}/`)
+	if (cb && isBrowser()) {
+		const win = window.open(webUrl, '_blank')
+		return win.focus()
+	}
+	return webUrl
+}
