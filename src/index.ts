f3aa755e4c476d8fddb70f08bf1a5fcfcf5c68cd
Ildar Timerbaev <ildar.timerbaev@piano.io>
2021-02-17 00:50:49 +0300

    Changed entrypoint file export defaults

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export * from './client'
-export * from './factories'
-export * from './methods'
+import { Client } from './client'
+import { Initialize } from './factories'
+import { sendOperation, sendOperations, sendTransaction, sign } from './methods'
+
+export default {
+	Client,
+	Initialize,
+	sign,
+	sendTransaction,
+	sendOperations,
+	sendOperation
+}
