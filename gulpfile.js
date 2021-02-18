96d85a264fab6e29b057bfdf4b031521e3d5ef84
Ildar Timerbaev <ildar.timerbaev@piano.io>
2021-02-18 14:53:56 +0300

    Get rid of rollup and compile browserify in gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var tsify = require('tsify');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+
+gulp.task("default", function () {
+    return browserify({
+        basedir: '.',
+        entries: ['src/index.ts'],
+        cache: {},
+        packageCache: {}
+    })
+        .plugin(tsify)
+        .bundle()
+        .pipe(source('hivesigner.min.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist'));
+});
