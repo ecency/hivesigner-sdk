2ea1bb26ee9eaeb43c41f2da9e49f609df40b281
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2021-02-28 12:28:36 +0300

    build

--- dist/hivesigner.js
+++ dist/hivesigner.js
@@ -4524,7 +4524,20 @@ exports.Initialize = Initialize;
 
 },{"../client":12}],19:[function(require,module,exports){
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./client"), exports);
+__exportStar(require("./methods"), exports);
+__exportStar(require("./factories"), exports);
 var client_1 = require("./client");
 var factories_1 = require("./factories");
 var methods_1 = require("./methods");
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,6 @@
+export * from './client';
+export * from './methods';
+export * from './factories';
 import { Client } from "./client";
 import { Initialize } from "./factories";
 import { sendTransaction, sendOperations, sendOperation, sign } from "./methods";
