9c6acff94fd9ee23b19f3636014763531c75a7b2
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2021-02-28 11:13:30 +0300

    default export for backward compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export * from './client'
-export * from './methods'
-export * from './factories'
+import {Client} from "./client";
+import {Initialize} from "./factories";
+import {sendTransaction, sendOperations, sendOperation, sign} from "./methods";
+
+export default {
+    Client,
+    Initialize,
+    sendTransaction,
+    sendOperations,
+    sendOperation,
+    sign,
+}
