3804a9d34b6bc612a16f1daa79e5848540712e90
Ildar Timerbaev <dkildar@gmail.com>
2021-03-08 22:02:46 +0300

    Added JS tests for require imports

---
+++ dist/methods/index.d.ts
@@ -0,0 +1,4 @@
+export * from './sign.method';
+export * from './send-operation.method';
+export * from './send-operations.method';
+export * from './send-transaction.method';
---
+++ dist/types/index.d.ts
@@ -0,0 +1,5 @@
+export * from './client-config.interface';
+export * from './sign-errors.interface';
+export * from './callback-function.type';
+export * from './login-options.interface';
+export * from './send-response.type';
